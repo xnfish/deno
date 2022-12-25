74fa5fa9ccdffd90d1e5e55944430fc2a668556e
xnfish <406116313@qq.com>
2022-12-25 20:38:39 +0800

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,15 @@
+import blog from "https://deno.land/x/blog/blog.tsx";
+
+blog({
+  author: "Dino",
+  title: "My Blog",
+  description: "The blog description.",
+  avatar: "https://deno-avatar.deno.dev/avatar/83a531.svg",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:bot@deno.com" },
+    { title: "GitHub", url: "https://github.com/denobot" },
+    { title: "Twitter", url: "https://twitter.com/denobot" },
+  ],
+  lang: "zh",
+});
